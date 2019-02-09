5ef4896219a33e18f329e8f656045c34bad3288c
junhong cheon <cjswnsghd12@gmail.com>
2019-02-09 16:16:18 +0900

    구조체 테스트

---
+++ StructTest/StructTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+struct Emp
+{
+    public int id;
+    public string name;
+}
+
+namespace StructTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Emp e;
+            e.id = 1;
+            e.name = "홍길동";
+            Console.WriteLine($"사번: {e.id}");
+            Console.WriteLine($"이름: {e.name}");
+
+            Emp e1 = new Emp();
+            e1.id = 2;
+            e1.name = "2길동";
+            Console.WriteLine($"사번: {e1.id}");
+            Console.WriteLine($"이름: {e1.name}");
+        }
+    }
+}

dd6ba0c0dea1c54c469333386d75bb03f4deea45
junhong cheon <cjswnsghd12@gmail.com>
2019-02-09 13:12:57 +0900

    TypeCastExample1

---
+++ TypeCastExample/TypeCastExample/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace TypeCastExample
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            short a = 10;
+            int b = a;
+
+            int c = 50000;
+
+            try
+            {
+                short d = checked((short)c);    //오버플로우 확인 checked()
+                Console.WriteLine("Short:{0}", d);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message); 
+                Console.WriteLine(e.StackTrace);
+                //Console.WriteLine("예외 : {0}", e.StackTrace);
+            }
+            
+        }
+    }
+}

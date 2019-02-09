0d1a071b77e20d6a76eff21a980058b25e87140f
junhong cheon <cjswnsghd12@gmail.com>
2019-02-09 18:15:26 +0900

    배열

---
+++ ArrTest/ArrTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[2];
+            int[] arr2 = new int[2] { 10, 20};
+            int[] arr3 = new int[] { 10, 20};
+            int[] arr4 = { 10, 20};
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine(arr4[i]);
+            }
+
+            for (int i = 0; i < arr3.Length; i++)
+            {
+                Console.WriteLine(arr3[i]);
+            }
+
+            foreach(int i in arr2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        
+    }
+}

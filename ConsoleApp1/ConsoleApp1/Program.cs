1094eb9b3ffdff81116d4c8d182cfc38b4d2df40
junhong cheon <cjswnsghd12@gmail.com>
2019-02-09 13:51:21 +0900

    Main 매개변수 출력 테스트

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello {0}, {1}", args[0], args[1]);
+        }
+    }
+}
